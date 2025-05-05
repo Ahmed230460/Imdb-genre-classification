0cc3c66176fd3520eb26c9b3fdd8c4d8e518fc24
Ahmed230460 <128981233+Ahmed230460@users.noreply.github.com>
2025-05-05 04:47:44 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-apt-get update && apt-get install -y python3-distutils python3-dev build-essential
+apt-get update && apt-get install -y unzip python3-distutils python3-dev build-essential
 pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
+unzip -o rf_model.zip  # Force overwrite if exists
